14a51f9f46d426b2785a3e1e8b4af3d1afbd8909
PEDRO_MARTINS <pedrofsmartins3@gmail.com>
2024-02-03 14:13:10 +0000

    showmorebtn

--- Pages/Projects.jsx
+++ Pages/Projects.jsx
@@ -1,10 +1,20 @@
+import React from "react"
 import allProjects from "../Components/Data/projectsData.json";
 import CreateProjects from "../Components/CreateProjects"
 import "../Components/Style/Projects.css"
 
 export default function Projects() {
 
-  const newArray = [...allProjects].reverse()
+  const [visible, setVisible] = React.useState(6)
+
+  function show() {
+    setVisible(prev => prev === 6 ? allProjects.length : 6)
+    console.log(visible)
+  }
+
+  const showBtnText = visible === 6 ? "Show more..." : "Show less..."
+
+  const newArray = [...allProjects].reverse().slice(0, visible)
 
   return (
     <main>
@@ -12,6 +22,7 @@ export default function Projects() {
       <section className="projects-container">
         <CreateProjects projects={newArray} />
       </section>
+      <button className="showmore" onClick={show}>{showBtnText}</button>
     </main>
   );
 }
